992f480199f3fa45a8d78da9ebda0967c0fa3eb8
minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-02-13 14:37:14 -0600

    declaracion de funciones para renombrar, listar y mover

--- core/actions.py
+++ core/actions.py
@@ -1,1 +1,28 @@
 # Mueve, renombra, comprime o lista archivos
+from pathlib import Path
+import re
+import os
+
+def rename(path_file : Path):
+    name, ext = os.path.splitext(path_file.name)
+    clean_name = re.sub(r'[^\w\s]', ' ', name)
+    clean_name = re.sub(r'\s+', ' ', clean_name).lower().strip()
+    final_name = clean_name.replace(" ", "_")
+    return f"{final_name}{ext}"
+
+
+def list_files(path_directory : Path):
+    if not path_directory.exists():
+        raise FileNotFoundError(IsADirectoryError)
+    for find_file in path_directory.iterdir():
+        if find_file.is_dir():
+            print(f"{path_directory.name}/{find_file.name}")
+            list_files(find_file)
+        if find_file.is_file():
+            print(f"File: {rename(find_file)}")
+
+def move_file(path_file : Path, path_destination : Path):
+    print(f"Moviendo {path_file.name} a {path_destination.name}")
+
+HOME = Path.home()
+print(list_files(HOME/ "Downloads"))
