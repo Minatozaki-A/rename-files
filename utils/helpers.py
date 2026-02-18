3e2700634c4dc720e78a03c454357c3643a86d62
minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-02-18 16:46:18 -0600

    add file size and timestamp helper functions

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,16 @@
 # Funciones de fechas y tamaños
+import datetime
+from pathlib import  Path
+
+def get_size(path_file : Path):
+    return path_file.stat().st_size
+
+def get_modification_time(path_file : Path):
+    return datetime.datetime.fromtimestamp(
+        path_file.stat().st_mtime
+    )
+
+def get_change_time(path_file : Path):
+    return datetime.datetime.fromtimestamp(
+        path_file.stat().st_ctime
+    )
