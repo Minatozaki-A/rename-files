47ed173ddaa2777df8467dd827ccb3bb9855819e
minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-02-18 16:26:42 -0600

    Refactor: modularizar sistema de organización de archivos con clasificación MIME

--- core/scanner.py
+++ core/scanner.py
@@ -1,29 +1,28 @@
 # Encuentra los archivos y obtiene metadatos
 from pathlib import  Path
-import hashlib as hashl
-import magic
-from core.actions import clean_name, rename, show_files
+from core.classifier import get_header
+from utils.helpers import get_size, get_modification_time, get_change_time
 
-def scan_file(path_file : Path, path_directory : Path):
-    ### esta logica se remplazara despues
-    name_file = clean_name(path_file)
-    final_path = path_directory
-    info = path_file.stat()
-    type_file = magic.from_file(path_file, mime=True)
-    hash_file = hashl.md5(path_file.read_bytes()).hexdigest()
-    size = info.st_size
-    last_modified = info.st_mtime
-    last_access = info.st_atime
-    metadata_change = info.st_ctime
-    print(f"{name_file} {type_file} {hash_file} {size} {last_modified} {last_access} {metadata_change}")
+def scan_file(path_file : Path):
+    header = get_header(path_file)
+    return {
+        "path": path_file,
+        "name": path_file.name,
+        "mime": header,
+        "extension": header.split("/")[1] if "/" in header else header,
+        "size": get_size(path_file),
+        "m_time": get_modification_time(path_file),
+        "c_time": get_change_time(path_file)
+    }
 
 
-
-def scan_directory(path_directory : Path):
+def scanner(path_directory : Path):
     if not path_directory.exists():
         raise FileNotFoundError(IsADirectoryError)
     for find_file in path_directory.iterdir():
         if find_file.is_file():
-            scan_file(find_file, path_directory)
+            scan_file(find_file)
+
         if find_file.is_dir():
-            scan_directory(find_file)
+            scanner(find_file)
+
