a2d941e81889d00c070267d34fd31bc831eaa0fc
minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-02-27 16:11:37 -0600

    refactor: move name cleaning functions from actions to text_utils

---
+++ utils/text_utils.py
@@ -0,0 +1,31 @@
+import unicodedata as udd
+import re
+from pathlib import Path
+
+def clean_name(name : str):
+    clean = udd.normalize('NFD', name)
+
+    without_accents = "".join(
+        c for c in clean if udd.category(c) != 'Mn'
+    )
+
+    clean = re.sub(r'[^\w\s]', ' ', without_accents)
+    clean = re.sub(r'\s+', ' ', clean).lower().strip()
+    final_name = re.sub(r'[\s_-]+', '-', clean)
+    # re.sub(r'-+', '-', clean.replace(" ", "-").replace("_", "-"))
+    return final_name
+
+def clean_file_name(path_file : Path):
+    name = path_file.stem.lower()
+    ext = path_file.suffix.lower()
+
+    final_name = clean_name(name)
+
+    return f"{final_name}{ext}"
+
+def clean_directory_name(filename: Path):
+    name = filename.stem
+    title = re.sub(r'^[\d\s-]+|[\d\s-]+', '', name,
+                flags=re.IGNORECASE).strip()
+    cleaned_title = clean_name(title)
+    return cleaned_title
