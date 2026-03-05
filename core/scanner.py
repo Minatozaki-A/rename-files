db4b3dfc8266d01b0fd83484c1ea0fb4a1ab1e22
minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-03-05 16:40:21 -0600

    refactor(scanner): rename function, adjust path handling, and update parameter usage

--- core/scanner.py
+++ core/scanner.py
@@ -1,9 +1,9 @@
 # Encuentra los archivos y obtiene metadatos
 from pathlib import  Path
-from config_loader import get_cached_config_value, build_directory_tree, save_structure_to_config
+from core.config_loader import get_cached_config_value, build_directory_tree, save_structure_to_config
 from utils.text_utils import clean_directory_name
 
-def scanner(path_directory: Path, config_path: Path = None):
+def scanner_structure_directories(path_directory: Path, config_path: Path = None, structure_path: Path = None):
     """Función principal que orquesta el mapeo y el guardado."""
     if not path_directory.exists() or not path_directory.is_dir():
         raise FileNotFoundError(f"La ruta no existe o no es un directorio: {path_directory}")
@@ -16,12 +16,13 @@ def scanner(path_directory: Path, config_path: Path = None):
             ignore_dir = cached_ignore
 
     # Construir la estructura empezando por el directorio raíz
+    path_directory_normalize = Path(str(path_directory).rstrip('/'))
     final_structure = {
-        clean_directory_name(path_directory): build_directory_tree(path_directory, ignore_dir)
+        clean_directory_name(path_directory_normalize): build_directory_tree(path_directory, ignore_dir)
     }
 
     # Guardar en config.json
     if config_path:
-        save_structure_to_config(config_path, final_structure)
+        save_structure_to_config(structure_path, final_structure)
 
     return final_structure
