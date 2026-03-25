6c314e593577974a30679c3f139a8fdc8b538b76
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-03-25 17:53:00 -0600

    [Refactorizacion] refactorizacion o cambios minimos en todas las funciones

--- core/actions.py
+++ core/actions.py
@@ -20,99 +20,94 @@ def find_ssd_mount():
             return Path(part.mountpoint)
     return None
 
+
 def resolve_name_file(path_file : Path):
     new_name = clean_file_name(path_file)
     if not new_name:
         raise ValueError(f"new name is empty: {path_file}")
 
-    if Path(new_name).stem == path_file.stem:
-        return None
-
     final_name = path_file.parent / new_name
+    name = final_name.stem
+    suffix = final_name.suffix
 
-    if not final_name.exists():
+    if  name == path_file.stem and suffix == path_file.suffix.lower():
         return final_name
 
-    name = Path(new_name).stem
-    suffix = Path(new_name).suffix
+    if final_name.exists() and final_name.samefile(path_file):
+        return final_name
+
     counter = 1
 
-    while True:
-        candidate = path_file.parent / f"{name}_({counter}){suffix}"
-        if not candidate.exists():
-            return candidate
+    temp_final = final_name
+    while temp_final.exists():
+
+        temp_final = path_file.parent / f"{name}-({counter}){suffix}"
         counter += 1
+
+    return temp_final
+
 
 def resolve_name_directory(path_dir: Path):
     new_name = clean_directory_name(path_dir)
     if not new_name:
         raise ValueError(f"new name is empty: {path_dir}")
 
-
-    if new_name == path_dir.name:
-        return None
-
     final_name = path_dir.parent / new_name
 
-    """if  final_name.exists():
-        raise FileExistsError(f"Directory already exists: {final_name}")
-"""
-    return final_name
+    if final_name.name == path_dir.name:
+        return final_name
 
-def _resolve_config(config_path: Path, config_value: list | None) -> list:
-    if config_value is not None:
-        return config_value
-    if config_path:
-        return get_cached_config_value(config_path, "ignore") or []
-    return []
+    if final_name.exists() and final_name.samefile(path_dir):
+        return final_name
 
-def get_name_files(source_path, config_path : Path, ignore_dir : list = None):
-    """ if ignore_dir is None and config_path:
-        ignore_dir = get_cached_config_value(config_path, "ignore") or []
-    elif ignore_dir is None:
-        ignore_dir = []"""
-    ignore_dir = _resolve_config(config_path, ignore_dir)
+    counter = 1
+    temp_final = final_name
+
+    while temp_final.exists():
+
+        temp_final = path_dir.parent / f"{new_name}-({counter})"
+        counter += 1
+
+    return temp_final
+
+
+def get_name_files(source_path, config_path : Path, key: str, ignore_dir : list = None):
+
+    ignore_dir = _resolve_config(config_path, key, ignore_dir)
     name_files = []
     for item in sorted(source_path.iterdir()):
         if item.is_dir():
             if item.name in ignore_dir:
                 continue
-            name_files.extend(get_name_files(item, config_path, ignore_dir))
+            name_files.extend(get_name_files(item, config_path, key, ignore_dir))
         elif item.is_file():
             name_files.append(item)
     return name_files
 
-def get_name_directories(source_path, config_path : Path, ignore_dir : list = None):
-    """if ignore_dir is None and config_path:
-        ignore_dir = get_cached_config_value(config_path, "ignore") or []
-    elif ignore_dir is None:
-        ignore_dir = []"""
-    ignore_dir = _resolve_config(config_path, ignore_dir)
+
+def get_name_directories(source_path, config_path : Path, key: str,ignore_dir : list = None):
+
+    ignore_dir = _resolve_config(config_path, key, ignore_dir)
     name_directories = []
     for item in sorted(source_path.iterdir()):
         if item.is_dir():
             if item.name in ignore_dir:
                 continue
             name_directories.append(item)
-            name_directories.extend(get_name_directories(item, config_path, ignore_dir))
+            name_directories.extend(get_name_directories(item, config_path, key, ignore_dir))
     return name_directories
 
 
 
-def show_files(path_directory, config_path: Path, depth: int = 0, ignore_dir: list = None):
+def show_files(path_directory, config_path: Path, key: str, depth: int = 0, ignore_dir: list = None):
     indent = "  " * depth
-    ignore_dir = _resolve_config(config_path, ignore_dir)
-    """if ignore_dir is None and config_path:
-        ignore_dir = get_cached_config_value(config_path, "ignore") or []
-    elif ignore_dir is None:
-        ignore_dir = []
-    print(f"{indent}[{clean_directory_name(path_directory)}]")"""
+    ignore_dir = _resolve_config(config_path, key, ignore_dir)
 
     for item in sorted(path_directory.iterdir()):
         if item.is_dir():
             if item.name in ignore_dir:
                 continue
-            show_files(item, config_path, depth + 1)
+            show_files(item, config_path, key, depth + 1)
 
         elif item.is_file():
             new_name = clean_file_name(item)
